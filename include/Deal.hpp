0170207218b0402762c6d3f28e196868f1f766e1
Chen-Pang He <chen.pang.he@jdh8.org>
2016-03-13 04:23:45 +0800

    'Upgrade' http to https to protect the tempted to follow links

--- include/Deal.hpp
+++ include/Deal.hpp
@@ -1,6 +1,6 @@
 // This file is part of Bridge, a library and utility for bridge.
 //
-// Copyright (C) 2016 Chen-Pang He <http://jdh8.org/>
+// Copyright (C) 2016 Chen-Pang He <https://jdh8.org/>
 //
 // Bridge is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef BRIDGE_DEAL_HPP
 #define BRIDGE_DEAL_HPP
