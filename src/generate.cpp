c9e79e378ddf325559868836b222bc7d57d6a786
Chen-Pang He <chen.pang.he@jdh8.org>
2022-08-09 16:11:43 +0800

    Hide unrelated LLVM options

--- src/generate.cpp
+++ src/generate.cpp
@@ -102,6 +102,7 @@ int main(int argc, char **argv)
 {
   using namespace llvm;
   cl::opt<std::size_t> number(cl::Positional, cl::desc("<number of deals>"), cl::init(10));
+  cl::HideUnrelatedOptions({});
 
   if (!cl::ParseCommandLineOptions(argc, argv, "Description?"))
     return 1;
